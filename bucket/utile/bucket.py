505bb13ddf6f22c4b1b768d390c784ce63425147
mohammadreza <mohammadrezasolo69@gmail.com>
2022-07-23 12:37:32 +0430

    config AWS in setting.py && create app bucket && create class Bucket

---
+++ bucket/utile/bucket.py
@@ -0,0 +1,15 @@
+import boto3
+from django.conf import settings
+
+
+class Bucket:
+    """ CDN Bucket manager """
+
+    def __init__(self):
+        session = boto3.session.Session()
+        self.conn = session.client(
+            service_name=settings.AWS_SERVICE_NAME,
+            aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+            endpoint_url=settings.AWS_S3_ENDPOINT_URL,
+        )
--- core/settings.py
+++ core/settings.py
@@ -29,8 +29,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # 3rd party
+    'storages',
 
     # my app
+    'bucket.apps.BucketConfig',
 ]
 
 MIDDLEWARE = [
@@ -86,7 +88,6 @@ elif env("ENGINE") == "postgresql":
         }
     }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
@@ -133,3 +134,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media', 'media_root')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# ARVAN CLOUD STORAGE
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+AWS_S3_ENDPOINT_URL = env('AWS_S3_ENDPOINT_URL')
+AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+AWS_SERVICE_NAME = env('AWS_SERVICE_NAME')
+AWS_S3_FILE_OVERWRITE = env('AWS_S3_FILE_OVERWRITE')
